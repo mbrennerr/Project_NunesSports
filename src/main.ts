e76140533bf78511975ebe91263668742f7718ef
mbrennerr <marciobrennerbusiness@gmail.com>
2024-02-04 03:18:24 -0300

    [refact] add_main_logic

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
-import './style/style.css'
-import {Table} from "./components/Table.ts";
+import "./style/style.css";
+import { Table } from "./components/Table.ts";
+import { Button } from "./components/Buttons.ts";
+import { getAllProducts } from "./utils/getAllProducts.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h2>Produtos</h2>
-    ${Table().outerHTML}
-  </div>
-`
+const app = document.querySelector<HTMLDivElement>("#app");
+if (app) {
+  const tableComponent = Table();
+  const buttonComponent = Button();
+
+  buttonComponent.addEventListener("click", () => {
+    getAllProducts()
+      .then((products) => {
+        const tbody = tableComponent.querySelector("tbody");
+        if (tbody) {
+          tbody.innerHTML = products
+            .map(
+              (product) => `
+                 <tr>
+                        <td>${product.cod}</td>
+                        <td>${product.name}</td>
+                        <td>${product.price}</td>
+                        <td>${product.description}</td>
+                 </tr>`,
+            )
+            .join("");
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao carregar produtos", error);
+      });
+  });
+  app.innerHTML = "";
+  app.appendChild(buttonComponent);
+  app.appendChild(tableComponent);
+}

9e92380391d6ccc4ef7de45ef5ab4fdfdcb05d8b
mbrennerr <marciobrennerbusiness@gmail.com>
2024-02-04 20:36:45 -0300

    [fix] fixing_get_endpoint_api

--- src/utils/getAllProducts.ts
+++ src/utils/getAllProducts.ts
@@ -2,15 +2,11 @@ import { Product } from "../types";
 import { API_URL } from "../api/apiProduct.ts";
 
 export async function getAllProducts(): Promise<Product[]> {
-  try {
-    const response = await API_URL.get<Product[]>("/product");
-    if (response.status === 200 && response.data) {
-      return response.data;
-    } else {
-      throw new Error("Erro na requisição da API");
-    }
-  } catch (error) {
-    console.error("Erro ao carregar produtos");
-    throw error;
+  const response = await API_URL.get<Product[]>("/products");
+  if (response.status === 200 && response.data) {
+    console.log(response.data, "console da função getAllProducts");
+    return response.data;
+  } else {
+    throw new Error("Erro na requisição da API");
   }
 }

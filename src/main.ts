7a7ac04308c8ce34b70a5b54ba7a629a0d919c4f
mbrennerr <marciobrennerbusiness@gmail.com>
2024-02-06 02:07:09 -0300

    [perf] updating_main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,34 @@
 import "./style/style.css";
 import { Table } from "./components/Table.ts";
-import { getAllProducts } from "./services/ProductService.ts";
 import { Button } from "./components/Buttons.ts";
-import { updateTable } from "./utils/updateTable.ts";
+import { Modal } from "./components/Modal.ts";
+import { createProductForm } from "./components/ProductForm.ts";
+// import { getAllProducts } from "./services/ProductService.ts";
+import { refreshProducts } from "./utils/refreshProducts.ts";
 
 const app = document.querySelector<HTMLDivElement>("#app");
 
 const init = async () => {
   if (app) {
-    const newProdButton = Button("Novo Produto", () => console.log("Create"));
+    const tableComponent = Table();
+
+    const newProdButton = Button("Novo Produto", () => {
+      const form = createProductForm(tableComponent, () => modal.close());
+      const modal = Modal(form);
+      modal.open();
+      console.log("newProdButton_log: Novo Produto clicado!");
+    });
     const updateProdButton = Button("Atualizar Produto", () =>
       console.log("Updated"),
     );
-    const tableComponent = Table();
 
     app.innerHTML = "";
     app.append(newProdButton, updateProdButton, tableComponent);
 
     try {
-      const products = await getAllProducts();
-      updateTable(tableComponent, products);
+      await refreshProducts(tableComponent);
+      // const products = await getAllProducts();
+      // updateTable(tableComponent, products);
     } catch (error) {
       console.error(error);
     }

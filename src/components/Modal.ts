5bf2acfd85b80c9aaf10a74ed2fb80a4306710eb
mbrennerr <marciobrennerbusiness@gmail.com>
2024-02-06 01:46:26 -0300

    [perf] add_modal_superpowers!

--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -1,6 +1,11 @@
+// import "../style/modal.css";
+
 export function Modal(content: HTMLElement) {
   const modalOverlay = document.createElement("div");
   modalOverlay.classList.add("modal-overlay");
+  modalOverlay.setAttribute("role", "dialog");
+  modalOverlay.setAttribute("aria-modal", "true");
+  modalOverlay.setAttribute("aria-hidden", "true");
 
   const modalContent = document.createElement("div");
   modalContent.classList.add("modal-content");
@@ -8,12 +13,25 @@ export function Modal(content: HTMLElement) {
   modalOverlay.appendChild(modalContent);
   function open() {
     modalOverlay.style.visibility = "visible";
+    modalOverlay.setAttribute("aria-hidden", "false");
+    document.body.appendChild(modalOverlay);
+    document.addEventListener("keydown", handleScapeClose);
   }
 
   function close() {
     modalOverlay.style.visibility = "hidden";
+    modalOverlay.setAttribute("aria-hidden", "true");
+    document.body.removeChild(modalOverlay);
+    document.removeEventListener("keydown", handleScapeClose);
   }
 
+  function handleScapeClose(event: KeyboardEvent) {
+    if (event.key === "Escape") {
+      close();
+    }
+  }
+
+  modalOverlay.addEventListener("click", close);
   modalContent.addEventListener("click", (event) => {
     event.stopPropagation();
   });

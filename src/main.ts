69205dde529be847108d2cadd5d521dbfad330fc
mbrennerr <marciobrennerbusiness@gmail.com>
2024-02-05 23:05:31 -0300

    [refact]decouple_the_updatetable_function

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,18 @@
 import "./style/style.css";
 import { Table } from "./components/Table.ts";
 import { getAllProducts } from "./services/ProductService.ts";
+import { Button } from "./components/Buttons.ts";
+import { updateTable } from "./utils/updateTable.ts";
 
 const app = document.querySelector<HTMLDivElement>("#app");
-const updateTable = (tableComponent: HTMLTableElement, products: any[]) => {
-  const tbody = tableComponent.querySelector("tbody");
-  if (tbody) {
-    tbody.innerHTML = products
-      .map(
-        (product) => `
-                     <tr>
-                            <td>${product.cod}</td>
-                            <td>${product.name}</td>
-                            <td>${product.price}</td>
-                            <td>${product.description}</td>
-                     </tr>`,
-      )
-      .join("");
-  }
-};
+
 const init = async () => {
   if (app) {
+    const createButtonComponent = Button(() => console.log("Create"));
     const tableComponent = Table();
-    //vou reutilizar essa função para criar um botão que cria um produto!
-    // const buttonComponent = Button(() => {
-    //   getAllProducts()
-    //       .then((products) => updateTable(tableComponent, products))
-    //       .catch((error) => console.error(error));
-    // });
+
     app.innerHTML = "";
-    app.append(tableComponent);
+    app.append(createButtonComponent, tableComponent);
 
     try {
       const products = await getAllProducts();
@@ -40,4 +23,7 @@ const init = async () => {
   }
 };
 
-init();
+document.addEventListener("DOMContentLoaded", async () => {
+  console.log("DOM fully loaded and parsed");
+  await init();
+});

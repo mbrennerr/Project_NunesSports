b2d466530d7a59ab6af91270e19bf648e027441c
mbrennerr <marciobrennerbusiness@gmail.com>
2024-02-07 23:51:05 -0300

    [feat]adicionando_metodo_update_service

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,6 +1,15 @@
 import { Product } from "../types";
 import { API_URL } from "../api/apiProduct";
 
+export const getAllProducts = async (): Promise<Product[]> => {
+  const response = await API_URL.get<Product[]>("/products");
+  if (response.status === 200 && response.data) {
+    // console.log(response.data, "Console_Service_getAllProducts");
+    return response.data;
+  } else {
+    throw new Error("Erro na requisição da API");
+  }
+};
 export const createProduct = async (product: Product) => {
   try {
     const response = await API_URL.post<Product>("/products", product);
@@ -19,18 +28,25 @@ export const createProduct = async (product: Product) => {
   }
 };
 
-export const getAllProducts = async (): Promise<Product[]> => {
-  const response = await API_URL.get<Product[]>("/products");
-  if (response.status === 200 && response.data) {
-    // console.log(response.data, "Console_Service_getAllProducts");
-    return response.data;
-  } else {
-    throw new Error("Erro na requisição da API");
-  }
-};
-
 export const updateProduct = async (product: Product) => {
-  console.log(product, "Console_Service_updateProduct");
+  try {
+    const response = await API_URL.put<Product>(
+      `/products/${product.id}`,
+      product,
+    );
+    if (response.status === 200) {
+      console.log("UpdateProduct_Log: Resposta da API", response.data);
+      return response.data;
+    } else {
+      console.error(
+        "MetodoUpdate_Else_Log:Resposta de Erro da API",
+        response.data,
+      );
+    }
+  } catch (error) {
+    console.error("MetodoUpdate_Catch_Log:Erro na requisição da API", error);
+    throw error;
+  }
 };
 
 export const deleteProduct = async (id: string) => {
